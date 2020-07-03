7ed342e8e60cf3e24536a4c8a02dd9d3517c1932
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-07-03 15:27:19 +0530

    Add files via upload
    Pattern programs

---
+++ PatterReverseTraingle.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number : "))
+k = 2*n - 2
+for i in range(0,n):
+    for j in range(0,k):
+        print(end="")
+    k=k-2
+    for j in range(0,k):
+        print('* ',end="")
+    print('\r')
+        
